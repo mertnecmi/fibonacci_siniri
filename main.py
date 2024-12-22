84389892362f7303c7d958bbee6f2828ab8a8c26
mertnecmi <necmimertnl@gmail.com>
2024-12-22 16:48:11 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#Soru 7: Fibonacci dizisini hesaplayan ve sonucu belirli bir sınıra kadar sayı
+#  içeren bir liste olarak döndüren bir döngü nasıl oluşturulur?
+
+sinir = int(input("Sınır Sayısını Giriniz : "))
+fibo_listesi = [0,1]
+
+for i in range(1,sinir):
+    yeni_sayi = fibo_listesi[-1]+fibo_listesi[-2]
+    if yeni_sayi > sinir:
+        break
+    fibo_listesi.append(yeni_sayi)
+
+print(fibo_listesi)
